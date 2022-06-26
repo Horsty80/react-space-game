e8f4146db35575a65c923bdcf083aa799c1a89aa
Cyril Hue <cy.hue80@gmail.com>
2022-06-26 15:21:19 +0200

    connect to room with context and service

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import styled from "styled-components";
 import { io } from "socket.io-client";
+import socketService from "./services/socketService";
 
 const AppContainer = styled.div`
   width: 100%;
@@ -13,22 +14,34 @@ const AppContainer = styled.div`
   padding: 1em;
 `;
 
-function App() {
-  const connect = () => {
-    const socket = io("http://localhost:9000");
+const WelcomeText = styled.div`
+  margin: 0;
+  color: #8e44ad;
+`;
 
-    socket.on("connect", () => {
-      socket.emit("custom_event", { name: "Cyril", age: 30 });
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+  const connectSocket = async () => {
+    const socket = await socketService.connect("http://localhost:9000").catch((err) => {
+      console.log("Error: ", err);
     });
   };
 
   useEffect(() => {
-    connect();
+    connectSocket();
   }, []);
-
+  
   return (
     <AppContainer>
-      <h1>Welcome to {import.meta.env.VITE_GAME_TITLE}</h1>
+      <WelcomeText>Welcome to {import.meta.env.VITE_GAME_TITLE}</WelcomeText>
+      <MainContainer>Hey</MainContainer>
     </AppContainer>
   );
 }
---
+++ src/contexts/gameContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export interface IGameContextProps {}
+
+const defaultState: IGameContextProps = {};
+
+export default createContext({});
